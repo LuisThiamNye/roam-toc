9597326e39c950dc5b51b355b7d74c6f2add564a
Luis Thiam-Nye <41270840+LuisThiamNye@users.noreply.github.com>
2021-03-09 21:05:53 +0000

    Create toc.cljs

---
+++ toc.cljs
@@ -0,0 +1,40 @@
+(ns toc
+  (:require
+   [clojure.string]
+   [roam.datascript.reactive :as dr]))
+
+(defn block-html-el [uid]
+  (some (fn [x]
+          (when (clojure.string/ends-with? (.-id x) uid) x))
+        (.getElementsByClassName js/document "rm-block__input")))
+
+(defn scroll-to-block [uid]
+  (when-some [el (block-html-el uid)]
+    (js/console.log el)
+    (.scrollIntoView el)))
+
+(defn flatten-block [acc block]
+  (reduce flatten-block
+          (conj acc (dissoc block :block/children))
+          (sort-by :block/order (:block/children block))))
+
+
+(defn component [{uid :block-uid}]
+  (into [:div]
+        (comp
+         (filter (fn [b]
+                   (-> b :block/heading pos?)))
+         (map (fn [b]
+                [:div {:style {:margin-left (str (dec (:block/heading b)) "em")
+                               :font-size (str (* 0.25 (- 7 (:block/heading b))) "em")}}
+                 [:a {:on-click (fn []
+                                  (scroll-to-block (:block/uid b)))}
+                  (:block/string b)]])))
+        (flatten-block []
+                       @(dr/q '[:find (pull ?p [:block/string :block/uid :block/heading :block/order {:block/children ...}]) .
+                                :in $ ?uid
+                                :where
+                                [?e :block/uid ?uid]
+                                [?e :block/parents ?p]
+                                [?p :node/title]]
+                              uid))))
