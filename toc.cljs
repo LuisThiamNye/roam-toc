56083bcc5ce28c2ff946db178a72d67a9d41accd
Luis Thiam-Nye <41270840+LuisThiamNye@users.noreply.github.com>
2021-06-14 14:46:47 +0100

    Add auto-expanding of blocks

--- toc.cljs
+++ toc.cljs
@@ -1,17 +1,36 @@
 (ns toc
   (:require
    [clojure.string]
+   [roam.block :as block]
+   [roam.datascript :as d]
    [roam.datascript.reactive :as dr]
    [roam.util :as u]))
+
+(defn ancestor-block-uids [uid]
+  (d/q '[:find [?puid ...]
+         :in $ ?cuid
+         :where
+         [?c :block/uid ?cuid]
+         [?c :block/parents ?p]
+         [?p :block/string]
+         [?p :block/uid ?puid]]
+       uid))
 
 (defn block-html-el [uid]
   (some (fn [x]
           (when (clojure.string/ends-with? (.-id x) uid) x))
         (.getElementsByClassName js/document "rm-block__input")))
 
-(defn scroll-to-block [uid]
+(defn try-scroll-into-view [uid]
   (when-some [el (block-html-el uid)]
-    (.scrollIntoView el)))
+      (.scrollIntoView el)))
+
+(defn scroll-to-block [uid]
+  (doseq [parent (ancestor-block-uids uid)]
+    (block/update
+     {:block {:uid parent :open true}}))
+  (when-not (try-scroll-into-view uid)
+    (js/setTimeout #(try-scroll-into-view uid) 300)))
 
 (defn flatten-block [acc block]
   (reduce flatten-block
@@ -19,22 +38,27 @@
           (sort-by :block/order (:block/children block))))
 
 
+(defn block->table-entry [block]
+  [:div {:style {:margin-left (str (dec (:block/heading block)) "em")
+                 :font-size (str (* 0.25 (- 7 (:block/heading block)))
+                                 "em")}}
+   [:a {:on-click #(scroll-to-block (:block/uid block))}
+    (u/parse (:block/string block))]])
+
 (defn component [{uid :block-uid}]
-  (into [:div]
-        (comp
-         (filter (fn [b]
-                   (-> b :block/heading pos?)))
-         (map (fn [b]
-                [:div {:style {:margin-left (str (dec (:block/heading b)) "em")
-                               :font-size (str (* 0.25 (- 7 (:block/heading b))) "em")}}
-                 [:a {:on-click (fn []
-                                  (scroll-to-block (:block/uid b)))}
-                  (u/parse (:block/string b))]])))
-        (flatten-block []
-                       @(dr/q '[:find (pull ?p [:block/string :block/uid :block/heading :block/order {:block/children ...}]) .
-                                :in $ ?uid
-                                :where
-                                [?e :block/uid ?uid]
-                                [?e :block/parents ?p]
-                                [?p :node/title]]
-                              uid))))
+  (let [block @(dr/q '[:find (pull ?p [:block/string
+                                       :block/uid
+                                       :block/heading
+                                       :block/order
+                                       {:block/children ...}]) .
+                       :in $ ?uid
+                       :where
+                       [?e :block/uid ?uid]
+                       [?e :block/parents ?p]
+                       [?p :node/title]]
+                     uid)]
+    (into [:div]
+          (comp
+           (filter #(-> % :block/heading pos?))
+           (map block->table-entry))
+          (flatten-block [] block))))
